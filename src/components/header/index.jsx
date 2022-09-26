27f719a0fd67cb2502e1da91c4d086bff771134e
god <2232341710@qq.com>
2022-09-26 16:52:27 +0800

    登录退出事件完成

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,12 +3,14 @@ import './index.less'
 import formateDate from '../../utils/dataUtils'
 import memoryUtils from '../../utils/memoryUtils'
 import { reqWeather } from '../../api'
-import { Divider, Button } from 'antd'
+import { Divider, Button, Modal } from 'antd'
 import menuList from '../../config/menuConfig'
 import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
-
+import { useLocation, useNavigate } from 'react-router-dom'
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import  storageUtils  from '../../utils/storageUtils'
 const date = new Date()
+const { confirm } = Modal;
 
 export default function Header() {
   const [reporttime, setReporttime] = useState() // 当前时间
@@ -19,7 +21,21 @@ export default function Header() {
   const [page, setPage] = useState() //当前页面
 
   const path = useLocation()
-
+  const navigate = useNavigate()
+  const showConfirm = () => {
+    confirm({
+      title: '你确定要退出吗？',
+      icon: <ExclamationCircleOutlined />,
+      onOk() {
+        console.log('OK');
+        // 删除保存的user数据
+        storageUtils.removeUser()
+        memoryUtils.user={}
+        // 跳转到login界面
+        navigate('/login')
+      }
+    });
+  };
   const getWeather = async () => {
     const { weather, temperature, winddirection, windpower } = await reqWeather()
     setWeather(weather); setTemperature(temperature + '°C'); setWinddirection(winddirection); setWindpower(windpower)
@@ -32,7 +48,7 @@ export default function Header() {
   }
   const getPage = () => {
     menuList.map((item) => {
-      if (item.key ===path.pathname) {
+      if (item.key === path.pathname) {
         setPage(item.title)
       }
       if (item.key === '/products' || item.key === '/charts') {
@@ -54,7 +70,7 @@ export default function Header() {
     <div className='header'>
       <div className="header-top">
         <span>欢迎，{memoryUtils.user.username}</span>
-        <Button type='link'>退出</Button>
+        <Button type='link' onClick={showConfirm}>退出</Button>
       </div>
       <div className="header-bottom">
         <div className="header-bottom-left">{page}</div>
